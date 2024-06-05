021d1255031d90a27a4af7d5e719b21318902a51
Chima Ejiofor <mobiblunt@gmail.com>
2024-06-05 17:17:35 +0100

    updated firestore

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,43 +1,76 @@
 import React, { useState, useEffect } from "react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, onValue, remove } from "firebase/database";
+
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  doc,
+  deleteDoc,
+  setDoc
+} from "firebase/firestore"
 import  myImage from "../../public/leaves.png";
 
-const appSettings = {
-    databaseURL: "https://shopping-database-e4b2a-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyDldaM3qYKq4hZUUCO0LYI-350TfJAjIEw",
+  authDomain: "shopping-6acd3.firebaseapp.com",
+  projectId: "shopping-6acd3",
+  storageBucket: "shopping-6acd3.appspot.com",
+  messagingSenderId: "672943075334",
+  appId: "1:672943075334:web:3b05fbb5844a85f0c31387"
+};
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const shoppingListInDB = ref(database, "shoppingList");
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+// const shoppingListInDB = ref(database, "shoppingList");
+const shoppingListInDB = collection(db, "shoppingList")
 
 function ShoppingList() {
   const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [list, setList] = useState(null)
 
-  const handleAddButtonClick = () => {
-    push(shoppingListInDB, inputValue);
-    setInputValue("");
+  const handleAddButtonClick = async () => {
+     
+    await addDoc(shoppingListInDB, list);
+    setInputValue([]);
+    setList(null)
+
   };
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
+    
+
+    const newNote = {
+      body: event.target.value,
+      createdAt: Date.now()
+  }
+  
+  setList(newNote)
+    
   };
 
   const handleItemRemove = (itemId) => {
-    const exactLocationOfItemInDB = ref(database, `shoppingList/${itemId}`);
-    remove(exactLocationOfItemInDB);
+    const exactLocationOfItemInDB = doc(db, "shoppingList", itemId);
+    deleteDoc(exactLocationOfItemInDB);
   };
-
+  console.log(items)
   useEffect(() => {
-    onValue(shoppingListInDB, (snapshot) => {
-      if (snapshot.exists()) {
-        const itemsArray = Object.entries(snapshot.val());
-        setItems(itemsArray);
-      } else {
-        setItems([]);
-      }
+    const unsubscribe = onSnapshot(shoppingListInDB, (snapshot) => {
+      
+        const notesArr = snapshot.docs.map(doc => ({
+          ...doc.data(),
+          id: doc.id
+      }))
+        setItems(notesArr);
+      
     });
+    
+    return unsubscribe
+    
   }, []);
 
   return (
@@ -51,9 +84,9 @@ function ShoppingList() {
       <button onClick={handleAddButtonClick}>Add</button>
       <ul>
         {items.length > 0 ? (
-          items.map(([itemId, itemValue]) => (
-            <li key={itemId} onClick={() => handleItemRemove(itemId)}>
-              {itemValue}
+          items.map((itemId) => (
+            <li key={itemId.id}  onClick={() => handleItemRemove(itemId.id)}>
+              {itemId.body}
             </li>
           ))
         ) : (
